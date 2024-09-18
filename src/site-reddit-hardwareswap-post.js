403c021afaedfb52cb594f77ef9b60d94d165c0c
GuacOn <seanwinters2@gmail.com>
2024-09-18 13:23:32 +1000

    feat: support for reddit hardwareswap posts

---
+++ src/site-reddit-hardwareswap-post.js
@@ -0,0 +1,62 @@
+let apiKey = chrome.storage.local.get("api_key");
+apiKey.then(onGot, onError);
+function onGot(item) {
+  console.debug(item["api_key"]);
+
+  console.log(`Extension ${manifest.name} v${manifest.version} starting...`);
+  redditReplaceTextByClass(
+    item["api_key"],
+    "block font-semibold text-neutral-content-strong m-0 visited:text-neutral-content-weak text-16 xs:text-18  mb-2xs xs:mb-xs ",
+  );
+}
+
+function onError(error) {
+  alert("Please set your OpenAI API key in the extension settings.");
+  console.log(`Error: ${error}`);
+}
+
+/**
+ * For each ad result, queries ad text + description, and supplements ad title with RTINGS data.
+ * @param {string} apiKey OpenAI API key (https://platform.openai.com/api-keys)
+ * @param {string} className Name of the class we will add information to
+ */
+async function redditReplaceTextByClass(apiKey, className) {
+  let elements = document.getElementsByClassName(className);
+  let tasks = [];
+
+  for (let i = 0; i < elements.length; i++) {
+    // for each ad element
+    let adTitle = elements[i].textContent + "\n";
+    let adDescription =
+      elements[i].parentElement.nextElementSibling.nextElementSibling
+        .children[0].textContent;
+
+    // Combine the two elements for our prompt
+    let gptPromptAdText = adTitle + adDescription;
+
+    // Send to GPT for analysis
+    tasks.push(
+      GPTFetchMonitorModel(apiKey, gptPromptAdText).then(
+        async function (value) {
+          console.log("Calling func retrieved model number: " + value);
+          // Search RTINGS API for monitor model
+          let result = await RTINGSSearch(value);
+          if (result) {
+            elements[i].textContent =
+              "[[ " + result + " ]] -- " + elements[i].textContent;
+          }
+        },
+        function (error) {
+          console.log("Calling func error: " + error);
+        },
+      ),
+    );
+
+    console.debug("Adding another task...");
+  }
+
+  // Wait for all API calls to complete
+  await Promise.all(tasks);
+
+  console.log(`${manifest.name} has finished replacing elements on this page.`);
+}
